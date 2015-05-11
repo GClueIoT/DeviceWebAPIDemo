2f7c5be347df1fd8902c148e47c06c32ea3cb1cf
小林伸郎 <nobu@gclue.jp>
2015-05-11 15:48:51 +0900

    デモ一覧画面において、デモの追加をjavascriptで行うように修正

--- js/contents/content-list.js
+++ js/contents/content-list.js
@@ -1,8 +1,60 @@
 (function() {
+  var lightDemo = {
+    name: 'light',
+    img: 'img/Appli_light.png',
+    title: 'ライト'
+  }
+  var heartRateDemo =  {
+    name: 'heartrate',
+    img: 'img/Appli_Heart.png',
+    title: '心拍計'
+  };
+  var hvccDemo =  {
+    name: 'hvcc',
+    img: 'img/Appli_Facial expression recognition.png',
+    title: '顔認識'
+  };
+
+  function isIOS() {
+    var ua = navigator.userAgent;
+    if(/iPhone/.test(ua)) {
+      return true;
+    } else if(/iPad/.test(ua)) {
+      return true;
+    }
+    return false;
+  }
+
   angular.module('demoweb')
-    .controller('demoListCtrl', ['$scope', '$window', '$location', 'demoWebClient', 'demoConstants', 'transition', function($scope, $window, $location, demoWebClient, demoConstants, transition) {
+    .controller('demoListCtrl', ['$scope', '$window', '$location', '$compile', 'demoWebClient', 'demoConstants', 'transition', function($scope, $window, $location, $compile, demoWebClient, demoConstants, transition) {
       transition.scope = $scope;
-      
+
+      var demos = [];
+      demos.push(lightDemo);
+      if (!isIOS()) {
+        demos.push(heartRateDemo);
+//        demos.push(hvccDemo);
+      }
+
+      var size = demos.length < 8 ? 4 : Math.ceil(demos.length / 2);
+      var index = 0;
+      for (var i = 0; i < size; i++) {
+        var t = '<tr>';
+        for (var j = 0; j < 2; j++) {
+          if (index < demos.length) {
+            t += '<td class="demo" ng-click="transit(\'' + demos[index].name + '\')">';
+            t += '<img class="logo" src="' + demos[index].img + '">';
+            t += '<span class="demo-name">' + demos[index].title + '</span>';
+            t += '</td>';
+          } else {
+            t += '<td class="nodemo"></td>';
+          }
+          index++;
+        }
+        t += '</tr>';
+        $('.demo-list').append($compile(t)($scope));
+      }
+
       $scope.title = 'デモ一覧';
       $scope.back = function() {
         $window.history.back();
